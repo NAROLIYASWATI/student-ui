657584fff3a12626cdd7cd91c895bdb8a1fa30a9
Swati Naroliya <swatinaroliya@gmail.com>
2018-07-22 14:05:45 +0530

    updated addSubmit

--- src/app/student/student-create/student-create.component.ts
+++ src/app/student/student-create/student-create.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { StudentService } from '../student.service';
+import { Student } from '../Student';
 
 @Component({
   selector: 'app-student-create',
@@ -8,82 +10,35 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class StudentCreateComponent implements OnInit {
 
-  addStudentForm: FormGroup;
-  submitted = false;
+  private student: Student;
+  private addStudentForm: FormGroup;
+  private submitted = false;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,private studentService: StudentService) { }
 
   ngOnInit() {
-      this.addStudentForm = this.formBuilder.group({
-          firstName: ['', Validators.required],
-          lastName: ['', Validators.required],
-          email: ['', [Validators.required, Validators.email]],
-          sid: ['', [Validators.required, Validators.minLength(2)]]
-      });
+    this.student=this.studentService.student;
+
+    this.addStudentForm = this.formBuilder.group({
+        sid: ['', [Validators.required, Validators.minLength(2)]],
+        fname: ['', Validators.required],
+        lname: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]]
+    });
   }
 
   // convenience getter for easy access to form fields
   get f() { return this.addStudentForm.controls; }
 
   onSubmit() {
-      this.submitted = true;
-
-      // stop here if form is invalid
-      if (this.addStudentForm.invalid) {
-          return;
-      }
-
-      alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.addStudentForm.value))
-  }
-
-
-  // addStudentForm: FormGroup;
-  
-  // submitted: boolean=false;
-
-  // private alertMessage="This is required";
-
-  // // private _student: Student;
-
-  // constructor(private studentService: StudentService,private formBuilder:FormBuilder) { 
-  // }
-
-  //  ngOnInit() {
-  // //    this._student=this.studentService.student;
-
-  //   this.addStudentForm = this.formBuilder.group({
-  //     sid: ['', [Validators.required]],
-  //     firstName: ['', Validators.required],
-  //     lastName: ['', Validators.required],
-  //     email: ['', [Validators.required, Validators.email]]
-  // });
-
-  //  }
-
- 
-
-
-  // addStudent(){
-  //   this.submitted =true;
-  //   // stop here if form is invalid
-  //   if (this.addStudentForm.invalid) {
-  //     return;
-  //   }
-
-  //   alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.addStudentForm.value))
-
-    
-  //   //this.sid=this._student.sid;
-  //   //this.fname=this._student.fname;
-  //   //this.lname=this._student.lname;
-
-  //  // this.studentService.student=this._student;
-  //   //this.studentService.addStudent();
-  // }
-
-  // // updateStudent(){
-  // //   this.studentService.student=this._student;
-  // //   this.studentService.updateStudent();
-  // // }
-
+        this.submitted = true;
+        if (this.addStudentForm.invalid) {
+            return;
+        }
+        
+        this.student=this.addStudentForm.value;
+        console.log(' final SUCCESS!! :- ' + JSON.stringify(this.student))
+        this.studentService.student=this.student;
+        this.studentService.addStudent();
+    }
 }
